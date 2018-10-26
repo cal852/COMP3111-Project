6da12b946c3c99c2258e04c67d1e1aae83d1abe2
enochwong3111 <enochywangdj@gmail.com>
2018-10-26 21:22:18 +0800

    Add refine button functions(enable, disable) Add fillTable function

--- src/main/java/comp3111/webscraper/Controller.java
+++ src/main/java/comp3111/webscraper/Controller.java
@@ -8,9 +8,12 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableCell;
@@ -19,6 +22,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Duration;
 
 
 /**
@@ -58,6 +62,9 @@ public class Controller {
     @FXML
     private Label labelRefineWarning;
     
+    @FXML
+    private Button refineBtn;
+    
     private WebScraper scraper;
         
     /**
@@ -72,19 +79,48 @@ public class Controller {
      */
     @FXML
     private void initialize() {
-    	
+    	disableRefine();
     }
 
     /**
      * Called when the search button is pressed.
      */
-    @SuppressWarnings({ "unchecked", "rawtypes" })
+    
 	@FXML
     private void actionSearch() {
     	System.out.println("actionSearch: " + textFieldKeyword.getText());
     	List<Item> result = scraper.scrape(textFieldKeyword.getText());
+    	if(!result.isEmpty()) {
+    		enableRefine();
+    	}else {
+    		disableRefine();
+    	}
     	String output = "";
-		TableColumn col1 = tableView1.getColumns().get(0);
+    	for (Item item : result) {
+    		output += item.getTitle() + "\t" + item.getPrice() + "\t" + item.getUrl() + "\n";
+    	}
+    	textAreaConsole.setText(output);
+    	fillTable();
+
+    	final ObservableList<Item> data = FXCollections.observableArrayList(result);
+		tableView1.setItems(data);
+    }
+    
+    /**
+     * Called when the new button is pressed. Very dummy action - print something in the command prompt.
+     */
+    @FXML
+    private void actionNew() {
+    	System.out.println("actionNew");
+    }
+    
+    /**
+     * Called when there are results ( > 0) after searching. Fill in the table content
+     */
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @FXML
+    private void fillTable() {
+    	TableColumn col1 = tableView1.getColumns().get(0);
     	TableColumn col2 = tableView1.getColumns().get(1);
     	TableColumn col3 = tableView1.getColumns().get(2);
     	TableColumn col4 = tableView1.getColumns().get(3);
@@ -109,31 +145,52 @@ public class Controller {
     	    };
     	    return cell;
     	});
-    	for (Item item : result) {
-    		output += item.getTitle() + "\t" + item.getPrice() + "\t" + item.getUrl() + "\n";
-    	}
-    	textAreaConsole.setText(output);
-
-    	final ObservableList<Item> data = FXCollections.observableArrayList(result);
-		tableView1.setItems(data);
-    }
-    
-    /**
-     * Called when the new button is pressed. Very dummy action - print something in the command prompt.
-     */
-    @FXML
-    private void actionNew() {
-    	System.out.println("actionNew");
     }
     
     /**
      * Called when the Refine button is pressed. Refine the search result
      */
+    Timeline timeline = new Timeline(new KeyFrame(
+	        Duration.millis(1000),
+	        ae -> labelRefineWarning.setVisible(false)));
     @FXML
-    void actionRefine() {
-    	//
+    private void actionRefine() {
+    	String refineWords = refineKeyword.getText();
+    	System.out.println("refineKeyword: " + refineWords + ", length: " + refineWords.length());
+    	int emptyNum = 0;
+    	for(int i = 0; i < refineWords.length(); i++) {
+    		if(refineWords.charAt(i) == ' ') {
+    			emptyNum++;
+    		}
+    	}
+    	if(refineWords.length() == 0 || emptyNum == refineWords.length()) {
+    		labelRefineWarning.setVisible(true);
+    		timeline.stop();
+    		timeline.play();
+    	}else {
+    		labelRefineWarning.setVisible(false);
+    		disableRefine();
+    	}
     }
     
+    /**
+     * Called when there are results ( > 0) after searching. Enable the Refine button and refine text field
+     */
+    @FXML
+    private void enableRefine() {
+    	refineBtn.setDisable(false);
+    	refineKeyword.setDisable(false);
+    }
+    
+
+    /**
+     * Called when initialize the program or there are results ( > 0) after searching. Disable the Refine button and refine text field
+     */
+    @FXML
+    private void disableRefine() {
+    	refineBtn.setDisable(true);
+    	refineKeyword.setDisable(true);
+    }
     
 }
 
