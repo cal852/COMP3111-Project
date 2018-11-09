d2a1105d6a28f8e2610509c520a57f8c80d6a8cb
enochwong3111 <enochywangdj@gmail.com>
2018-11-09 21:43:25 +0800

    Add test case for Item, fail for the hyperlink test(Don't know why)

--- src/test/java/comp3111/webscraper/ItemTest.java
+++ src/test/java/comp3111/webscraper/ItemTest.java
@@ -2,7 +2,15 @@ package comp3111.webscraper;
 
 
 import org.junit.Test;
+
+import javafx.scene.control.Hyperlink;
+import junit.framework.Assert;
+
 import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 
 public class ItemTest {
@@ -12,5 +20,39 @@ public class ItemTest {
 		Item i = new Item();
 		i.setTitle("ABCDE");
 		assertEquals(i.getTitle(), "ABCDE");
+	}
+	
+	@Test
+	public void testSetPrice() {
+		Item i = new Item();
+		i.setPrice(50.0);
+		assertEquals(i.getPrice(), 50.0, 0);
+	}
+	
+	@Test
+	public void testSetUrl() {
+		Item i = new Item();
+		i.setUrl("https://course.cse.ust.hk/comp3111/");
+		assertEquals(i.getUrl(), "https://course.cse.ust.hk/comp3111/");
+	}
+	
+	@Test
+	public void testSetDate() throws ParseException {
+		Item i = new Item();
+		i.setDate("Nov 11");
+		SimpleDateFormat format = new SimpleDateFormat("MMM dd", Locale.US);
+		assertEquals(format.format(i.getDate()), "Nov 11");
+	}
+	
+	@Test
+	//I don't know why how it fails _Enoch
+	public void testSetLinkUrl() {
+		Item i = new Item();
+		i.setLinkUrl("https://course.cse.ust.hk/comp3111/");
+		Hyperlink url = i.getLinkUrl();
+		String url2 = url.toString();
+        url2 = url2.substring(url2.indexOf("]'") + 2, url2.length()-1);
+        System.out.println("Test link:"+ url2);
+		assertEquals(url2, "https://course.cse.ust.hk/comp3111/");
 	}
 }
